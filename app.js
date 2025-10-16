f9374f7690ba007b0ea9b0033740e82a6ae3d334
Anton Belski <anton.belski@gmail.com>
2025-10-16 11:23:40 +0300

    refactor(basic-3.4header-image): replace img tag selector with class-based selector and update CSS/JS

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ var createNewTaskElement = function (taskString) {
 
   deleteButton.className = "button delete";
   deleteButtonImg.src = "./remove.svg";
+  deleteButtonImg.className = "remove";
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
